2a74f2412ea363cb1fe91ac7072f9d44257edea7
mrdavinski <117495419+mrdavinski@users.noreply.github.com>
2022-11-08 10:09:04 +0100

    Done with exercies

--- event-practice/main.js
+++ event-practice/main.js
@@ -1,4 +1,23 @@
+function alertFunction(){
+    alert ("good work buddu");
+}
+
+
 const btn = document.querySelector("#btn");
-btn.addEventListener("click", () =>{
-    alert("Hello World");
+
+btn.addEventListener('click', function (e) {
+    e.target.style.background = "blue";
+  });
+
+const buttons = document.querySelectorAll("button");
+
+
+buttons.forEach((button) => {
+
+button.addEventListener("click", () => alert(button.id));
+
 });
+
+
+
+let isPositive = number => number >=0;
